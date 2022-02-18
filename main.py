ddb4e851af547d8b84bde98a904efc32d8bc89c0
Your Name <you@example.com>
2022-02-18 15:28:34 +0300

    Список профессий

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/training/<prof>')
-def index(prof):
+def training(prof):
     return render_template('training.html', prof=prof.lower())
+
+
+@app.route('/list_prof/<lst>')
+def list_prof(lst):
+    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                   'инженер по терраформированию', 'климатолог',
+                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                   'штурман', 'пилот дронов']
+    return render_template('list_prof.html', list=lst, professions=professions)
 
 
 if __name__ == '__main__':
