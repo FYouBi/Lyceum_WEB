140841e3ca78adcbd4f4d3eaf96e0ef48f230ffa
Your Name <you@example.com>
2022-03-17 16:13:56 +0300

    Получение всех работ

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template, url_for, request, redirect
+import flask
+from flask import Flask, render_template, url_for, request, redirect, jsonify
 import datetime
 from data.jobs import Jobs
 from forms import LoginForm, Protection, LoadPhoto, RegisterForm
 from data import db_session
 from data.users import User
+from requests import get
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+blueprint = flask.Blueprint(
+    'jobs_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/api/jobs')
+def get_news():
+    db_sess = db_session.create_session()
+    news = db_sess.query(Jobs).all()
+    return jsonify(
+        {
+            'jobs':
+                [item.to_dict(only=('id', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished', 'team_leader'))
+                 for item in news]
+        }
+    )
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -45,4 +66,5 @@ def authorization():
 if __name__ == '__main__':
     db_session.global_init('db/users.db')
     session = db_session.create_session()
+    app.register_blueprint(blueprint)
     app.run(port=8080, host='127.0.0.1')
